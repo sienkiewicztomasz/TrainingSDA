afe5778d93de22c1c55af41222d32efbef7a337c
Tomek <tsjagiellonia@gmail.com>
2018-10-14 23:38:03 +0200

    Done!

---
+++ PictureOfStars/src/main/java/Stars/Stars1.java
@@ -0,0 +1,16 @@
+package Stars;
+
+public class Stars1 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 7; i++) {
+            System.out.println();
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+
+
+            }
+        }
+    }
+}
