988a3198a70956caba8069df59fef483cada96f0
Tomek <tsjagiellonia@gmail.com>
2018-10-16 21:02:07 +0200

    Done!

--- PictureOfStars/src/main/java/Stars/Stars4.java
+++ PictureOfStars/src/main/java/Stars/Stars4.java
@@ -4,21 +4,19 @@ public class Stars4 {
 
 
     public static void main(String[] args) {
-        int i,j,x,z;
-        int y=6;
-        for(i=0; i<y; i++) {
-            x=2*i+1;
-            z=(2*y+1-x)/2;
-            for(j=0; j<z; j++) {
-                System.out.print(" ");
+        for (int i = 1; i < 7; i++) {
+            for (int j = 0; j <7; j++) {
+                if (i==j) {
+                    System.out.print("*");
+                } else if (i+j == 7){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
-            for(j=0; j<x; j++) {
-                System.out.print("*");
-            }
-            System.out.println("\n");
+            System.out.println();
+        }
     }
-}
-
 }
 
 
