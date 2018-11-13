b2444e65ea0221f9fbb74a05df30df1eabf33c56
Tomasz Sienkiewicz <tsjagiellonia@gmail.com>
2018-11-13 14:11:57 +0100

    SPOJ Training

--- Spoj/src/main/java/Nursery.java
+++ Spoj/src/main/java/Nursery.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 
 public class Nursery {
 
+    public static int greatestCommonDivisor(int a, int b) {
+        while (a != b) {
+            if (a > b) {
+                a -= b;
+            } else {
+                b -= a;
+            }
+        }
+        return a;
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Enter the number (1-10): ");
@@ -16,28 +27,9 @@ public class Nursery {
             int b = random1.nextInt(29) + 1;
             System.out.println(b);
 
-            int temp = 1;
+            int NWW = a * b / greatestCommonDivisor(a, b);
 
-            if (a == b) {
-                temp = a;
-                System.out.println(temp);
-            } else {
-                if (a > b) {
-                    while (temp % b == 0) {
-                        for (int j = 1; j < b + 1; j++) {
-                            temp = j * a;
-                        }
-                    }
-                    System.out.println(temp);
-                } else {
-                    while (temp % a == 0) {
-                        for (int j = 1; j < a + 1; j++) {
-                            temp = j * b;
-                        }
-                        System.out.println(temp);
-                    }
-                }
-            }
+            System.out.println("The nursery needs " + NWW + " candy.");
         }
     }
 }
